99a0e6fbcbc5ec5339b2f941cc4577c7690d04fb
JuliaSuhovici <63416383+JuliaSuhovici@users.noreply.github.com>
2020-04-30 11:00:19 +0300

    Update ParkingApplication.java

--- src/ParkingApplication.java
+++ src/ParkingApplication.java
@@ -3,9 +3,9 @@ public class ParkingApplication {
 
 	public static void main(String[] args) {
 		Parking parkingPlaces = new Parking();
-		Car c1 = new Car("BMW");
-		Car c2 = new Car("Fiat");
-		Car c3 = new Car("Mercedes");
+		Car c1 = new Car("BMW", "ABC-123");
+		Car c2 = new Car("Fiat", "HIL-513");
+		Car c3 = new Car("Mercedes", "WAT - 000");
 				
 		System.out.println("c1.print(): " + c1.print());
 		System.out.println("c2.print(): " + c2.print());
@@ -13,9 +13,9 @@ public class ParkingApplication {
 		
 		System.out.println("Total free parking places:");
 		parkingPlaces.printFreePlaces();
-		parkingPlaces.parkCar(c1,"1A");
-		parkingPlaces.parkCar(c2,"2C");
-		parkingPlaces.parkCar(c3,"1A"); //ERROR: OCCUPIED
+		parkingPlaces.parkCar(c1,"2A");
+		parkingPlaces.parkCar(c2,"1C");
+		parkingPlaces.parkCar(c3,"2A"); //ERROR: OCCUPIED
 		parkingPlaces.printMap();
 		System.out.println("Free parking places:");
 		parkingPlaces.printFreePlaces();
